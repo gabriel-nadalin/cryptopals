0bf7fe50b5f7e5b4af3de0acc8807aa9908a8535
gabriel-nadalin <gknadalin@gmail.com>
2023-10-27 01:28:23 -0300

    desafio 3 completo

--- set1.py
+++ set1.py
@@ -1,10 +1,11 @@
 import asyncio
 import base64
 import math
+import string
 from collections import Counter
 
-#table source: https://github.com/Lukasa/cryptopals/blob/master/cryptopals/challenge_one/three.py
-FREQUENCY_TABLE = {
+
+englishFreq = {
     b'a':  0.08167,
     b'b':  0.01492,
     b'c':  0.02782,
@@ -32,7 +33,8 @@ FREQUENCY_TABLE = {
     b'y':  0.01974,
     b'z':  0.00074,
 }
-
+if isinstance(b'a'[0], int):
+    englishFreq = {x[0]: y for x, y in englishFreq.items()}
 
 def hex2base64(hex):
     b = bytes.fromhex(hex)
@@ -45,24 +47,70 @@ def xor(a, b):
 def sxor(a, byte):
 	return bytes(i ^ byte for i in a)
 
-def score_frequency(a):
-	f = Counter(a.lower())
-	length = len(a)
+#retorna verdadeiro caso a string de bytes contenha apenas caracteres printaveis
+def isPrintable(bytestring):
+	result = True
+	for char in bytestring:
+		try:
+			dchar = char.to_bytes().decode()
+		except:
+			return False
+		result = result and (dchar in string.printable)
+	return result
 
-	return sum((n - FREQUENCY_TABLE.get(char, 0) * length)**2 / FREQUENCY_TABLE.get(char, 0) * length for char, n in f.items())
+
+#utiliza o teste qui-quadrado para comparar a frequencia de caracteres
+#de um input 'a' com a frequencia esperada na lingua inglesa
+#quanto menor o qui-quadrado maior a proximidade da amostra com o esperado
+def scoreFrequency(a):
+	frequency = Counter(a.lower())          #gera um dicionario com caracter nas chaves e frequencia nos valores
+	length = len(a)
+	chi2 = 0
+	for char in englishFreq:
+		observed = frequency.get(char, 0)
+		expected = englishFreq[char] * length
+		chi2 += (observed - expected)**2 / expected
+	return chi2
+
 
 #challenge 1
-#print(hex2base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
+
+'''
+hex = ("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+b64 = hex2base64(hex)
+print("hex = ", hex)
+print("b64 = ", b64)
+'''
 
 #challenge 2
-#print(bytes.fromhex("1c0111001f010100061a024b53535009181c"), bytes.fromhex("686974207468652062756c6c277320657965"))
-#print(xor(bytes.fromhex("1c0111001f010100061a024b53535009181c"), bytes.fromhex("686974207468652062756c6c277320657965")))
-#print(bytes.fromhex("746865206b696420646f6e277420706c6179"))
+
+'''
+a = bytes.fromhex("1c0111001f010100061a024b53535009181c")
+b = bytes.fromhex("686974207468652062756c6c277320657965")
+expected = bytes.fromhex("746865206b696420646f6e277420706c6179")
+result = xor(a, b)
+print("a = ", a, ", b = ", b)
+print("a xor b = ", result)
+print("expected = ", expected)
+'''
 
 #challenge 3
-results = ((sxor(bytes.fromhex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"), byte), byte) for byte in range(256))
-print(str(results))
-emap = [(score_frequency(r[0]), r[0], r[1]) for r in results]
-emap.sort(key=lambda x: x[0], reverse=True)
-for i in range(5):
-	print(emap[i])
+
+hex = bytes.fromhex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+results = [sxor(hex, byte) for byte in range(256)]
+printable = {}
+
+#seleciona apenas os resultados printaveis
+for r in results:
+	if isPrintable(r):
+	    printable[scoreFrequency(r)] = r
+
+#ordem crescente de qui-quadrado
+printable = dict(sorted(printable.items()))
+
+#5 menores qui-quadrados
+top5 = {k: printable[k] for k in list(printable)[:5]} 
+
+print("5 most likely results:")
+for i in top5:
+	print(i, top5[i])
